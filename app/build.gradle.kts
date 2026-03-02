b170b7481c6e799cbd1e20ac9c218305a1352339
Muhammad Usman <msusman97@gmail.com>
2026-03-02 21:27:03 +0500

    - Release version 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.talsk.amadz"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
